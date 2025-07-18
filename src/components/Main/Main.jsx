c432ee81c227809eec3716a25a8443df0e34cc54
HarshitaGon <harshitagon@gmail.com>
2025-07-18 22:27:52 +0530

    new commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -19,7 +19,7 @@ const Main = () => {
                 {!showResult
                     ? <>
                         <div className="greet">
-                            <p><span>Hello, Dev.</span></p>
+                            <p><span>Hello, User.</span></p>
                             <p>How can I help you today?</p>
                         </div>
 
@@ -52,12 +52,12 @@ const Main = () => {
                             <img src={assets.gemini_icon} alt="" />
 
                             {loading
-                            ?<div className="loader">
-                                <hr />
-                                <hr />
-                                <hr />
-                            </div>
-                            :<p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                                ? <div className="loader">
+                                    <hr />
+                                    <hr />
+                                    <hr />
+                                </div>
+                                : <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
                             }
                         </div>
                     </div>
@@ -70,7 +70,7 @@ const Main = () => {
                         <div>
                             <img src={assets.gallery_icon} alt="" />
                             <img src={assets.mic_icon} alt="" />
-                            {input?<img onClick={() => onSent()} src={assets.send_icon} alt="" />:null}
+                            {input ? <img onClick={() => onSent()} src={assets.send_icon} alt="" /> : null}
                         </div>
                     </div>
                     <p className="bottom-info">
