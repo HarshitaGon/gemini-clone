abe54565a375d523c0775f73680fc88ed9577733
HarshitaGon <harshitagon@gmail.com>
2025-07-18 19:58:29 +0530

    functionality commits

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -3,47 +3,52 @@
 // npm install -D @types/node
 
 import {
-  GoogleGenAI,
+    GoogleGenAI,
 } from '@google/genai';
 
 async function main(prompt) {
-  const ai = new GoogleGenAI({
-    apiKey: import.meta.env.VITE_GEMINI_API_KEY,
-  });
-  const tools = [
-    {
-      googleSearch: {
-      }
-    },
-  ];
-  const config = {
-    thinkingConfig: {
-      thinkingBudget: -1,
-    },
-    tools,
-    responseMimeType: 'text/plain',
-  };
-  const model = 'gemini-2.5-pro';
-  const contents = [
-    {
-      role: 'user',
-      parts: [
+    const ai = new GoogleGenAI({
+        apiKey: import.meta.env.VITE_GEMINI_API_KEY,
+    });
+    const tools = [
         {
-          text: `prompt`,
+            googleSearch: {
+            }
         },
-      ],
-    },
-  ];
+    ];
+    const config = {
+        thinkingConfig: {
+            thinkingBudget: -1,
+        },
+        tools,
+        responseMimeType: 'text/plain',
+    };
+    const model = 'gemini-2.5-pro';
+    const contents = [
+        {
+            role: 'user',
+            parts: [
+                {
+                    text: prompt,
+                },
+            ],
+        },
+    ];
 
-  const response = await ai.models.generateContentStream({
-    model,
-    config,
-    contents,
-  });
-  let fileIndex = 0;
-  for await (const chunk of response) {
-    console.log(chunk.text);
-  }
+    const response = await ai.models.generateContentStream({
+        model,
+        config,
+        contents,
+    });
+
+    let fullText = "";
+
+    for await (const chunk of response) {
+        console.log(chunk.text);
+        fullText += chunk.text;
+    }
+
+    return fullText;
 }
 
 export default main;
